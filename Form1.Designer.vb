82646561029ea6d8617d7da95ae3ba0a92fec59a
Jonathan Rogers <jonathanr1rogersjonathan@outlook.com>
2020-09-03 17:33:27 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.lblDataEntryTitle = New System.Windows.Forms.Label()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RestartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProgramToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearAllItemsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblBorder = New System.Windows.Forms.Label()
+        Me.lblDataReviewTitle = New System.Windows.Forms.Label()
+        Me.lblItemNumberSentence = New System.Windows.Forms.Label()
+        Me.lstData = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblDataEntryTitle
+        '
+        Me.lblDataEntryTitle.AutoSize = True
+        Me.lblDataEntryTitle.Location = New System.Drawing.Point(12, 44)
+        Me.lblDataEntryTitle.Name = "lblDataEntryTitle"
+        Me.lblDataEntryTitle.Size = New System.Drawing.Size(57, 13)
+        Me.lblDataEntryTitle.TabIndex = 4
+        Me.lblDataEntryTitle.Text = "Data Entry"
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnUpdate.Location = New System.Drawing.Point(37, 152)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(108, 38)
+        Me.btnUpdate.TabIndex = 9
+        Me.btnUpdate.Text = "Update Data"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.ProgramToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(693, 24)
+        Me.MenuStrip1.TabIndex = 10
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RestartToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'RestartToolStripMenuItem
+        '
+        Me.RestartToolStripMenuItem.Name = "RestartToolStripMenuItem"
+        Me.RestartToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RestartToolStripMenuItem.Text = "&Restart"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'ProgramToolStripMenuItem
+        '
+        Me.ProgramToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearAllItemsToolStripMenuItem})
+        Me.ProgramToolStripMenuItem.Name = "ProgramToolStripMenuItem"
+        Me.ProgramToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.ProgramToolStripMenuItem.Text = "&Program"
+        '
+        'ClearAllItemsToolStripMenuItem
+        '
+        Me.ClearAllItemsToolStripMenuItem.Name = "ClearAllItemsToolStripMenuItem"
+        Me.ClearAllItemsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ClearAllItemsToolStripMenuItem.Text = "&Clear All Items"
+        '
+        'lblBorder
+        '
+        Me.lblBorder.AutoSize = True
+        Me.lblBorder.Location = New System.Drawing.Point(179, 32)
+        Me.lblBorder.Name = "lblBorder"
+        Me.lblBorder.Size = New System.Drawing.Size(9, 299)
+        Me.lblBorder.TabIndex = 15
+        Me.lblBorder.Text = "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "|" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblDataReviewTitle
+        '
+        Me.lblDataReviewTitle.AutoSize = True
+        Me.lblDataReviewTitle.Location = New System.Drawing.Point(201, 44)
+        Me.lblDataReviewTitle.Name = "lblDataReviewTitle"
+        Me.lblDataReviewTitle.Size = New System.Drawing.Size(69, 13)
+        Me.lblDataReviewTitle.TabIndex = 16
+        Me.lblDataReviewTitle.Text = "Data Review"
+        '
+        'lblItemNumberSentence
+        '
+        Me.lblItemNumberSentence.AutoSize = True
+        Me.lblItemNumberSentence.Location = New System.Drawing.Point(201, 305)
+        Me.lblItemNumberSentence.Name = "lblItemNumberSentence"
+        Me.lblItemNumberSentence.Size = New System.Drawing.Size(478, 26)
+        Me.lblItemNumberSentence.TabIndex = 17
+        Me.lblItemNumberSentence.Text = "This concludes the inventory printout. ____ items have been displayed for a total" &
+    " inventory " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "value of $_____. __ .  The most valuable item in the inventory is _" &
+    "______ at a value of $_____. __ ."
+        '
+        'lstData
+        '
+        Me.lstData.FormattingEnabled = True
+        Me.lstData.Location = New System.Drawing.Point(204, 68)
+        Me.lstData.Name = "lstData"
+        Me.lstData.Size = New System.Drawing.Size(475, 225)
+        Me.lstData.TabIndex = 18
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(693, 342)
+        Me.Controls.Add(Me.lstData)
+        Me.Controls.Add(Me.lblItemNumberSentence)
+        Me.Controls.Add(Me.lblDataReviewTitle)
+        Me.Controls.Add(Me.lblBorder)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.lblDataEntryTitle)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Inventory Manager"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblDataEntryTitle As Label
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RestartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents lblBorder As Label
+    Friend WithEvents lblDataReviewTitle As Label
+    Friend WithEvents lblItemNumberSentence As Label
+    Friend WithEvents lstData As ListBox
+    Friend WithEvents ProgramToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearAllItemsToolStripMenuItem As ToolStripMenuItem
+End Class
